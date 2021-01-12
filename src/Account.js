ee7a01e7bb7d9d7650a44b691387aeabc378cc83
Samantha <samanthagottlieb31@gmail.com>
2021-01-12 12:42:56 +0000

    Add deposit method to Account class

--- src/Account.js
+++ src/Account.js
@@ -2,6 +2,10 @@ class Account {
   constructor(balance = 0) {
     this.balance = balance;
   }
+
+  deposit(amount) {
+    this.balance += amount;
+  }
 }
 
 module.exports = Account;
--- src/Account.spec.js
+++ src/Account.spec.js
@@ -1,9 +1,14 @@
 const Account = require("./Account");
 
 describe("Account", () => {
-  let account = new Account();
-
   it("has a starting balance of zero", () => {
+    let account = new Account();
     expect(account.balance).toEqual(0);
+  });
+
+  it("allows the user to make a deposit", () => {
+    let account = new Account();
+    account.deposit(100);
+    expect(account.balance).toEqual(100);
   });
 });
