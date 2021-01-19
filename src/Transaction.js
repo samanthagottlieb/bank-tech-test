1c5b40a6367ecfab615f79e41d31d9ab653a735d
Samantha <samanthagottlieb31@gmail.com>
2021-01-19 23:14:13 +0000

    Add locale argument to Date method

--- src/Transaction.js
+++ src/Transaction.js
@@ -8,7 +8,7 @@ class Transaction {
   _getDepositDetails() {
     let newBalance = (this.currentBalance += this.amount);
     let depositDetails = {
-      date: this.date.toLocaleDateString(),
+      date: this.date.toLocaleDateString("en-GB"),
       credit: this.amount.toFixed(2),
       debit: "",
       balance: newBalance.toFixed(2),
@@ -20,7 +20,7 @@ class Transaction {
   _getWithdrawalDetails() {
     let newBalance = (this.currentBalance -= this.amount);
     let withdrawalDetails = {
-      date: this.date.toLocaleDateString(),
+      date: this.date.toLocaleDateString("en-GB"),
       credit: "",
       debit: this.amount.toFixed(2),
       balance: newBalance.toFixed(2),
